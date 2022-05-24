81323cd18ea31c70c7560911cf4c1ba70f9ff191
AndonG7 <gjorgjiev.andon@uklo.edu.mk>
2022-05-24 02:28:50 +0200

    Prva verzija nesortirani vektori od ascii kod

---
+++ main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int najgolemaCifra(int indeks) {
+    int arr[3];
+    int najgolem=0;
+
+    arr[0] = indeks / 100;
+    arr[1] = indeks % 100; arr[1] = arr[1] / 10;
+    arr[2] = indeks % 10;
+
+    for (unsigned int i=0; i<4; i++){
+        if (arr[i]>najgolem) najgolem=arr[i];
+    }
+
+    return najgolem;
+}
+
+
+int main()
+{
+    int indeks,broj;
+    const string sodrzina = "Andon";
+    vector<int> encrypted;
+
+    cout<<"Vasiot indeks e: INKI";
+    cin>>indeks;
+    broj = najgolemaCifra(indeks);
+
+    for (unsigned int i=0; i<sodrzina.size(); i++) {
+        encrypted.push_back((int) sodrzina.at(i) + broj);
+    }
+
+    for (unsigned int i=0; i<encrypted.size(); i++) {
+        cout<<encrypted[i]<<" ";
+    }
+
+    return 0;
+}
